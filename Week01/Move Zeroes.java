009b9da2fa365776b3c6517b82e58dfc4b967f59
Ryan <865833921@qq.com>
2020-08-17 23:16:08 +0800

    Update Move Zeroes.java

--- Week01/Move Zeroes.java
+++ Week01/Move Zeroes.java
@@ -43,3 +43,21 @@ class Solution {
         
     }
 }
+
+
+//解法三：
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for(int i = 0;i < nums.length;i ++ ) {
+            if(nums[i] != 0) {
+                nums[j] = nums[i];
+                if(i != j) {
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+        
+    }
+}
